b2ac979a65e600e895d17420caa7b8f8ec7c487b
Your Namegit <zimohuang0@gmail.com>
2025-09-15 15:51:22 -0400

    data types

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,7 +1,7 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
+    // TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(int[] numbers) {
-        int s = 0; // variable to accumulate the sum in!
+        long s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
